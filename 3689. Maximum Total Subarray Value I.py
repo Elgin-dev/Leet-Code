08751a7edadbc90bc44d097dc94b30b0697e1e70
Elgin-dev <ebelgineuginraj2006@gmail.com>
2026-06-09 12:20:43 +0530

    today tricked leetcode

--- 3689. Maximum Total Subarray Value I.py
+++ 3689. Maximum Total Subarray Value I.py
@@ -11,4 +11,11 @@ class Solution:
 
 
 
-        
+
+# alternative
+
+class Solution:
+    def maxTotalValue(self, nums: List[int], k: int) -> int:
+        maxx = max(nums)
+        minn = min(nums)
+        return (maxx - minn) * k
